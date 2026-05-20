c1c3ba43eb7ea28ba9d1a0693584bb51d0d360fd
maks <maks-zych@wp.pl>
2026-05-20 08:30:15 +0200

    początkowe pliki

---
+++ steps.py
@@ -0,0 +1,15 @@
+def matrix_reduction():
+    #TODO: implement matrix reduction
+    pass
+
+def section_ij():
+    # TODO: Wyznaczenie odcinka <i\*j\*> o max optymistycznym koszcie wyłączenia (spośród wszystkich aij=O)
+    pass
+
+
+def split_problem():
+    # TODO: Podział problemu na dwa podproblemy: z aij=1 i z aij=0
+    # Wyznaczenie odcinka <i\*j\*> o max optymistycznym koszcie wyłączenia (spośród wszystkich aij=O)
+    pass
+
+def 
